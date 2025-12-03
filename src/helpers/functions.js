808f679cf7f48389139db662849347bcd67a3ba1
TusharKumarpolyvalent <tusharkumar@polyvalent.co.in>
2025-12-03 12:51:44 +0530

    fix auth

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -8,8 +8,8 @@ export const checkAuth = (navigate) => {
   const loggedIn = localStorage.getItem('loggedIn');
   if (loggedIn === 'true') {
     const loggedInUser = JSON.parse(localStorage.getItem('crm_user'));
-    if (loggedInUser.user.role === 'agent') navigate('/agent/dashboard');
-    else if (loggedInUser.user.role === 'admin') navigate('/admin/dashboard');
+    if (loggedInUser?.user?.role === 'agent') navigate('/agent/dashboard');
+    else if (loggedInUser?.user?.role === 'admin') navigate('/admin/dashboard');
   } else {
     navigate('/');
   }
